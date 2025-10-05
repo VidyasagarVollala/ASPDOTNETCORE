3c5655ef0895de4e96b3e1acd898c3626504de42
Vidyasagar <Admin@DESKTOP-3ONUBFN>
2025-10-05 19:00:09 +0530

    Response_Request_Querystring_ResponseHeader

--- SECTION_1/Program.cs
+++ SECTION_1/Program.cs
@@ -7,9 +7,35 @@ namespace SECTION_1
 			var builder = WebApplication.CreateBuilder(args);
 			var app = builder.Build();
 
-			app.MapGet("/", () => "Hello World!");
+			app.Run(async (HttpContext httpContext) =>
+			{
 
+				//Response
+				//httpContext.Response.StatusCode = 500;
+				//httpContext.Response.Headers["MyKey"] = "MySecreat_Key";
+				//httpContext.Response.ContentType = "text/html";
+				//await httpContext.Response.WriteAsync("<h3>Hello</h3>");
+				//await httpContext.Response.WriteAsync(" DotnetCore World");
+
+				//Request
+				//httpContext.Response.ContentType = "text/html";
+				//await httpContext.Response.WriteAsync($"<h3>{httpContext.Request.Path}</h3>");
+
+				//QueryString
+				//httpContext.Response.ContentType = "text/html";
+				//if(httpContext.Request.Method =="GET")
+				//{
+				//	if (httpContext.Request.Query.ContainsKey("id"))
+				//		await httpContext.Response.WriteAsync($"<h2>{httpContext.Request.Query["id"]}</h2>");
+				//}
+				httpContext.Response.ContentType = "text/html";
+				if (httpContext.Request.Headers.ContainsKey("user-agent"))
+					await httpContext.Response.WriteAsync($"<h1>{httpContext.Request.Headers["user-agent"]}</h1>");
+				
+			});
 			app.Run();
+
 		}
 	}
 }
+	
