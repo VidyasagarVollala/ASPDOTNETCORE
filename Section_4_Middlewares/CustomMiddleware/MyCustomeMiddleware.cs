8e139871ce9b77be0cca249a2b7f9efe1d2eed43
Vidyasagar <Admin@DESKTOP-3ONUBFN>
2025-10-10 06:42:14 +0530

    End_Of_Middleware_Section

---
+++ Section_4_Middlewares/CustomMiddleware/MyCustomeMiddleware.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Section_4_Middlewares.CustomMiddleware
+{
+	public class MyCustomeMiddleware : IMiddleware
+	{
+		public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+		{
+			await context.Response.WriteAsync("\n customMiddleware Begin");
+			await next(context);
+			await context.Response.WriteAsync("\n customMiddleware End");
+		}
+
+		
+	}
+	//Here we are extending the IApplicationBuilder object 
+	//In program.cs if we go to implementation we can see the WebApplication
+	//WebApplication  is the child of IApplicationBuilder so if we extend the IApplicationBuilder automatically that is injected into app object
+	public static class CustomMiddlewareExtension
+	{
+		public static IApplicationBuilder UseMyCustomMiddleware(this IApplicationBuilder app)
+		{
+			return app.UseMiddleware<MyCustomeMiddleware>();
+		}
+	}
+}
