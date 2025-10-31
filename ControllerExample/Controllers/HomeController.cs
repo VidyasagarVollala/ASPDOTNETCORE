1357c7817449f67326a7353361f173b0b63487c8
Vidyasagar <Admin@DESKTOP-3ONUBFN>
2025-10-31 08:54:51 +0530

    AddControllers_MapControllers

---
+++ ControllerExample/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerExample.Controllers
+{
+	public class HomeController 
+	{
+		[Route("index")]
+		[Route("/")] //Indicates Default route
+		public string Index()
+		{
+			return "Hello Sindhu_Devamshika from Index ";
+		}
+
+		[Route("about")]
+		public string About()
+		{
+			return "Hello Sindhu_Devamshika from About ";
+		}
+
+		[Route("contact-us")]
+		public string Contact()
+		{
+			return "Hello Sindhu_Devamshika from Contact ";
+		}
+
+		[Route("byEmpId/{id:int}")] //Routing constrains like int, bool
+		public string Employee()
+		{
+			return "Hello Sindhu_Devamshika from Employee ";
+		}
+
+
+	}
+}
---
+++ ControllerExample/Program.cs
@@ -0,0 +1,31 @@
+using ControllerExample.Controllers;
+
+namespace ControllerExample
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+			//in real world we have lot of controllers so instead of registering every controller as service so it can participate in dependency injection like this we can use below method
+			//builder.Services.AddTransient<HomeController>();
+			//A service class is resuable class
+			//In asp core controllers also treated as services
+
+			
+			builder.Services.AddControllers();
+			//Adds the all controllers as in the IServiceCollection.So that they can be accessed when a specific endpoint need it
+			// with this asp.dotnet core willl detects the all controllers- What are classes suffix with controller it will add
+			//Adds the all the controller classes as services
+
+
+
+			var app = builder.Build();
+
+			app.MapControllers();
+			//Adds all actions methods as endpoints. So that no need of using UseEndpoints() method for adding the action methods as endpoints
+			//It will detects the all controllers of enitre project and it will pick up all action methods and routing will be adding at a time.by this
+			app.Run();
+		}
+	}
+}
