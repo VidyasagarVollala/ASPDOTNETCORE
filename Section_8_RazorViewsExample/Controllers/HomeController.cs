4fdf19d7524070319a1b8408a2ac650c4aec640d
Vidyasagar <Admin@DESKTOP-3ONUBFN>
2025-11-16 21:50:47 +0530

    Strongly Typed views

---
+++ Section_8_RazorViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Section_8_RazorViewsExample.Model;
+
+namespace Section_8_RazorViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+
+            List<Person> people = new List<Person>
+    {
+    new Person()
+    {
+        Name="Vidyasagar", Age=29,Gender=Gender.Male, SurName="Vollala"
+    }, new Person()
+    {
+        Name="Sindhuja", Age=25,Gender=Gender.Female, SurName="Vollala"
+    }, new Person()
+    {
+        Name="Devamshika", Age=1,Gender=Gender.Female, SurName="Vollala"
+    }
+
+    };
+            ViewData["appTtitle"] = "Welcome to dotnet core view";
+
+
+            // ViewData["person"] = people;
+
+            // ViewBag.people = people;
+
+            //strongly typed view example
+            return View("Index", people);
+        }
+        //[Route("person-product")]
+
+        //public IActionResult PersonWithProduct()
+        //{
+        //    Person people = new Person() { Name = "Vidyasagar", Age = 29, Gender = Gender.Male, SurName = "Vollala" };
+
+
+        // Product product = new Product()
+        // {
+        //     Id = 1, Description = "Mobile"
+        // };
+           
+        //    PersonDataWithProduct personDataWithProduct = new PersonDataWithProduct() { PersonData = people, ProductData = product };
+        //    return View("PersonWithProduct",personDataWithProduct);
+        //}
+
+
+        [Route("person-product2")]
+
+        public IActionResult PersonWithProduct2()
+        {
+            List<Person> people  = new List<Person>
+            {
+    new Person()
+    {
+        Name="Vidyasagar", Age=29,Gender=Gender.Male, SurName="Vollala"
+    }, new Person()
+    {
+        Name="Sindhuja", Age=25,Gender=Gender.Female, SurName="Vollala"
+    }, new Person()
+    {
+        Name="Devamshika", Age=1,Gender=Gender.Female, SurName="Vollala"
+    }
+
+    };
+
+
+            List<Product> products = new List<Product>()
+           {
+               new Product ()
+               {
+                   Id=1, Description="Mobile"
+               }, new Product()
+               {
+                   Id=1, Description="Laptop"
+               }
+           };
+
+            PersonDataWithProduct personDataWithProduct = new PersonDataWithProduct() { PersonData = people , ProductData = products };
+            return View("PersonWithProduct", personDataWithProduct);
+        }
+    }
+}
---
+++ Section_8_RazorViewsExample/Program.cs
@@ -0,0 +1,16 @@
+namespace Section_8_RazorViewsExample
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+			builder.Services.AddControllersWithViews();
+			var app = builder.Build();
+
+			app.UseStaticFiles();
+			app.MapControllers();
+			app.Run();
+		}
+	}
+}
