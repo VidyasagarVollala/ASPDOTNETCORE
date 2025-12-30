21c5d6ee742a54a5291a19a8176b78fb5d4e312a
Vidyasagar <Admin@DESKTOP-3ONUBFN>
2025-12-30 11:00:21 +0530

    DI_ExampleEnding_Section_12

---
+++ Section_12_Services/CitiesServices.cs
@@ -0,0 +1,33 @@
+﻿using ServiceContracts;
+
+namespace Section_12_Services
+{
+    public class CitiesServices :ICitiesServices, IDisposable
+    {
+        private List<string>   _cities;
+        private Guid _instanceId;
+
+        public Guid InstanceId { get { return _instanceId; } }
+
+        public CitiesServices()
+        {
+            _instanceId = Guid.NewGuid();
+            _cities = new List<string>()
+            {
+                "Hyderabad",
+                "Karimnagar",
+                "Jagityal"
+            };
+        }
+
+        public List<string> GetCities()
+        {
+            return _cities;
+        }
+
+        public void Dispose()
+        {
+            //
+        }
+    }
+}
